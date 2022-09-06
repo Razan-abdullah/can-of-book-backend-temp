3b135edcb8dccfe24f06f50ad84bbd293127c43f
Razan-abdullah <razanaalabdallah017@gmail.com>
2022-09-06 04:44:07 +0300

    k

--- server.js
+++ server.js
@@ -12,42 +12,56 @@ server.use(cors()); //make my server open for any request
 
 //IP : http://localhost:PORT
 
-const PORT = process.env.PORT || 3010;
+const PORT = process.env.PORT || 3100;
 
 // mongoose config
-mongoose.connect('mongodb://localhost:27017/301d35-cats', {useNewUrlParser: true, useUnifiedTopology: true}); // 1 - connect mongoose with DB (301d35-cats)
+mongoose.connect('mongodb://localhost:27017/books', {useNewUrlParser: true, useUnifiedTopology: true}); // 1 - connect mongoose with DB (301d35-cats)
 
 const kittySchema = new mongoose.Schema({ //define the schema (structure)
     title: String,
-    descriptions:String,
-    status:String,
+    des: String,
+    status:String
   });
 
 const KittenModel = mongoose.model('Kitten', kittySchema); //compile the schem into a model
 
 //seed data (insert initial data)
-    await book1.save();
+async function seedData(){
+    const firstbook = new KittenModel({
+        title :"JS book ",
+        des:"for beginner",
+        status:"favorite five"
+    })
 
+    const secondbook = new KittenModel({
+      title :"green life ",
+      des:"for beginner",
+      status:"favorite five"
+    })
+
+    const thirdbook = new KittenModel({
+      title :"behind the seen ",
+      des:"Self development",
+      status:"life changing "
+    })
+
+    await firstbook.save();
+    await secondbook.save();
+    await thirdbook.save();
+}
 
 // seedData(); //call seedData function
 
 
-//Routes
+//Routes 
 server.get('/',homeHandler);
-server.get('/books',bookHandler);
-server.get('/getCats',getCatsHandler);
+
+server.get('/getbooks',getbooksHandler);
 server.get('*',defualtHandler);
 
 
-// http://localhost:3010/
-function homeHandler(req,res) {
-    res.send("Hi from the home route");
-}
 
-// http://localhost:3010/test
-function bookHandler(req,res) {
-    res.status(200).send("You are requesting the test route");
-}
+
 
 // http://localhost:3010/*
 function defualtHandler(req,res) {
@@ -55,7 +69,14 @@ function defualtHandler(req,res) {
 }
 
 
-function getCatsHandler(req,res) {
+function homeHandler(req,res){
+
+  res.status(200).send("Welcom in Home Rout ")
+
+
+}
+
+function getbooksHandler(req,res) {
     KittenModel.find({},(err,result)=>{
         if(err)
         {
